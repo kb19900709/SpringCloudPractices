69d6a5e5684399a52ebd2923d9a311e5640def2a
KB <kb19900709@gmail.com>
2018-02-02 18:35:16 +0800

    the first commit

---
+++ eureka_client/src/main/java/com/cathay/kb/practice/eureka_client/ApplicationMain.java
@@ -0,0 +1,22 @@
+package com.cathay.kb.practice.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+
+    @RequestMapping("/theFirstFunction")
+    public String theFirstFunction() {
+        return "Spring in cloud";
+    }
+}
