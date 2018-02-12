564326093d2dcfcc2b2f945748ff8e253648c31d
KB <kb19900709@gmail.com>
2018-02-12 18:35:05 +0800

    remove import of no need

--- zuul/src/main/java/com/cathay/kb/practice/zuul/filter/AccessFilter.java
+++ zuul/src/main/java/com/cathay/kb/practice/zuul/filter/AccessFilter.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class AccessFilter extends ZuulFilter {
